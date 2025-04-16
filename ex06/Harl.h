5f69535ddb2c50acba320a394bb7ff0fa650ec3f
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-04-16 11:32:00 +0200

    initial setup for ex06

---
+++ ex06/Harl.h
@@ -0,0 +1,26 @@
+#ifndef HARL_H
+#define HARL_H
+
+#include <string>
+#include <iostream>
+
+enum class complainLevel {
+  //
+};
+
+class Harl {
+  public:
+    Harl();
+    ~Harl();
+    void filter( std::string level );
+    void complain( std::string level );
+    typedef void ( Harl::*fcnPtr )();
+
+  private:
+    void debug( void );
+    void info( void );
+    void warning( void );
+    void error( void );
+};
+
+#endif
---
+++ ex06/main.cpp
@@ -0,0 +1,14 @@
+#include "Harl.h"
+
+int main(int argc, char **argv) {
+    Harl harl;
+
+    if (argc != 1 || argc != 2) {
+        std::cout << "The program takes either no or one argument" << '\n';
+        return 1;
+    }
+
+    //
+
+    return 0;
+}
